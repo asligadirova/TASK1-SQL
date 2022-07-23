31743da6814b7d574a1df6acdfa0cfdb5cb792a2
asligadirova <97395144+asligadirova@users.noreply.github.com>
2022-07-23 11:28:35 +0400

    Add files via upload

---
+++ TASK1.2.sql
@@ -0,0 +1,103 @@
+﻿-- Cədvəl :
+--İşçi - (SAA, VəzifəId,Maaş)
+--Vəzifə - (Ad)
+--Filial - (Ad)
+--Məhsul - (Ad, Alış qiyməti, Satış qiyməti)
+--Satış - (MəhsulId, İşçiId, Satış tarixi)
+---Satış cədvəlində işçilərin , satılan məhsulların, satışın olduğu filialın, məhsulun alış və satış qiyməti yazılsın.
+
+
+USE Blog
+CREATE Table İşçi(
+ID INT Primary Key identity,
+AD NVARCHAR (25) NOT NULL,
+SOYAD NVARCHAR (25) NOT NULL,
+MAAŞ DEC(18,2),
+VəzifəID INT REFERENCES Vəzifə(ID)
+)
+INSERT INTO İşçi (AD, SOYAD, MAAŞ, VəzifəID)
+Values
+('Asli', 'Qedirova', 1800, 1 ),
+('Fərid', 'İsgendorov', 12800, 2),
+('Guler', 'Ehmedova', 890, 3),
+('Zəhra' , 'Quluyeva', 5600, 4),
+('Ali', 'Aliyev', 7900, 5),
+('Qurban', 'Qurbanov', 1670, 6)
+
+
+CREATE TABLE Vəzifə(
+ID INT Primary Key identity,
+AD NVARCHAR (150) NOT NULL
+)
+INSERT INTO Vəzifə
+Values 
+('Satis Meneceri'),
+('Filial mudiri'),
+('Satici'),
+('Bas kassir'),
+('Anbardar')
+INSERT INTO Vəzifə
+Values 
+('Nezaretci')
+
+Select * from Vəzifə
+
+
+CREATE TABLE Filial(
+ID INT Primary Key identity,
+AD NVARCHAR (50) NOT NULL
+)
+ INSERT INTO Filial
+ Values
+ ('Yasamal'),
+ ('Buzovna'),
+ ('Guneshli'),
+ ('Bineqedi'),
+ ('Sabirabad'),
+ ('Goycay')
+
+CREATE TABLE Məhsul(
+ID INT Primary Key identity,
+AD NVARCHAR (50) NOT NULL,
+AlışQiyməti DEC (18,2) NOT NULL,
+SatışQiyməti DEC (18,2) NOT NULL
+)
+ INSERT INTO Məhsul(AD, AlışQiyməti, SatışQiyməti)
+ Values
+ ('Alma', 2, 2.50),
+ ('Nar', 5, 6),
+ ('Heyva', 3.5, 4),
+ ('Gavali', 3, 5),
+ ('Ciyelek', 5, 6),
+ ('Gilas', 3, 4)
+
+ Select * From Məhsul
+
+CREATE TABLE Satış(
+ID INT Primary Key identity,
+MəhsulId INT REFERENCES Məhsul(ID),
+İşçiId INT REFERENCES İşçi(ID),
+FilialID INT REFERENCES Filial(ID),
+SatışTarixi date ,
+)
+
+INSERT INTO Satış (MəhsulId,İşçiId ,FilialID, SatışTarixi)
+Values
+(1, 1, 1, '2022-07-01')
+INSERT INTO Satış (MəhsulId,İşçiId ,FilialID, SatışTarixi)
+Values
+(2, 1, 1, '2022-06-03')
+INSERT INTO Satış (MəhsulId,İşçiId ,FilialID, SatışTarixi)
+Values
+(2, 2, 2, '2022-05-01'),
+(3, 3, 3, '2022-05-11'),
+(4, 3, 3, '2022-07-13')
+
+INSERT INTO Satış (MəhsulId,İşçiId ,FilialID, SatışTarixi)
+Values
+(2, 1, 1, '2022-07-01')
+Select * from Satış
+
+Select * from Satış  as s
+LEFT JOIN Məhsul as m
+ON m.SatışQiyməti=s.MəhsulId
---
+++ TASK1.sql
@@ -0,0 +1,88 @@
+﻿ --Blog saytı databazası qurursunuz
+  --Saytda istifadəçilər (Users table), paylaşılan postlar (Posts table) .  Hər post bir istifadəçiyə aiddir. Bir istifadəçi istədiyi sayda post paylaşa bilər.
+--Bu databza sturukturunu dbdesigner saytında qrurub screenini və databazanı, içindəki table-ları yaradan DDL commandlarınız və eyni zamanda hər table-a ən az 5-10 data insert etdiyiniz DML commandlarınızın olduğu sql saylını github reponuza yerləşdirib linki atırsınız.
+
+
+--Table sturuklarını özünüz quracaqsınız.Sadəcə aşağıdakı columnlar mütləq olmalıdır.Aşağıdakılardan əlavə columnlar artıra bilərsiniz istəsəniz.
+
+
+--Users-da Name,Surname,Email
+--Posts-da LikeCount,CreatedAt, Title, UserId
+
+
+--Databazanın commandlarını yazarkən aşağıdakıları nəzərə alın:
+--Post-un mətni 500-dən uzun ola bilməz və null ola bilməz
+--User-in Email dəyəri təkrarlana bilməz və null ola bilməz və uzunluğu 4-100 aralığında olmalıdır--
+
+CREATE DATABASE Blog
+USE Blog
+
+CREATE TABLE Users(
+ID INT Primary Key identity,
+Name NVARCHAR(25),
+Surname NVARCHAR(25),
+Email NVARCHAR  Not Null Constraint CK_EMAIL CHECK(LEN(Email)>=4), CHECK(LEN(Email)<=100)
+)
+
+
+ALTER TABLE Users
+Drop Constraint CK_EMAIL
+ALTER TABLE Users
+Add Constraint CK_EMAIL CHECK((LEN(Email)>=4) AND (LEN(Email)<=100))
+
+ALTER TABLE Users
+Drop Constraint CK__Users__Email__37A5467C
+
+ALTER TABLE Users
+Drop Column Email 
+ALTER TABLE Users
+Add Email NVARCHAR(200)
+
+INSERT INTO Users
+Values
+( 'Asli', 'Gadirova', 'gadirova@mail.com'),
+( 'Yusif', 'Gadirov', 'yusif@gmail.com'),
+( 'Turan', 'Əhmədova', 'turan@gmail.com'),
+( 'Asim', 'Vəliyev', 'asim@gmail.com'),
+( 'Banu', 'Əliyeva' ,'banu@gmail.com'),
+( 'Ləman', 'Haciyeva' ,'banu@gmail.com')
+
+
+CREATE TABLE Posts(
+ID INT Primary Key identity,
+Title NVARCHAR (150),
+PostText  NVARCHAR(250) NOT NULL,  Constraint CK_PT CHECK(LEN(PostText)<500) ,
+LikeCount INT,
+CreatedAt date DEFAULT getdate(),
+UserId INT references Users(ID)
+)
+
+
+INSERT INTO Posts (Title,PostText, LikeCount,CreatedAt, UserId)
+Values
+('Saglamliq', 'Idman etmək sağlamlığa faydalıdır', 45, '2022-07-22', 14)
+INSERT INTO Posts (Title,PostText, LikeCount,CreatedAt, UserId)
+Values
+('C vitamini', 'Sitrus meyveleri C vitamini ilə zəngindir', 56, '2022-07-22', 17),
+('Texnologiya', 'Müasir dövrdə texnologiya həyatımızın ayrılmaz hissəsinə cevrilib', 122,'2022-07-22', 18),
+('IT', 'IT sahesine gunumuzde boyuk tələb var',67,'2022-07-22', 19),
+('Code academy', 'Intensiv tədrisə üstünlük verir', 643, '2022-07-22', 20),
+('Netflix', 'Muxtelif janrda kinolarinin platforma',67, '2022-07-22',21)
+
+
+
+CREATE TABLE UserPost(
+ID INT Primary Key identity,
+PostID INT references Posts(ID),
+UserID INT references Users(ID)
+)
+INSERT INTO UserPost ( PostID, UserID)
+VALUES 
+(8, 14)
+INSERT INTO UserPost ( PostID, UserID)
+VALUES 
+(9, 17),
+(10, 18),
+(11, 19),
+(12, 20),
+(13, 21)
